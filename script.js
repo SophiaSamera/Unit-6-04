a64a9974546cf0c3fa94d4f9e2e760ebf0d00985
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-18 18:27:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('calculate').addEventListener('click', calculateArea)
+
+let h = 0
+let a = 0
+let b = 0
+
+function calculateArea () {
+  h = document.getElementById('insert1').value
+  a = document.getElementById('insert2').value
+  b = document.getElementById('insert3').value
+
+  h = parseInt(h)
+  a = parseInt(a)
+  b = parseInt(b)
+
+  number = (a+b)/2*h
+  document.getElementById('answer').value = number
+}
